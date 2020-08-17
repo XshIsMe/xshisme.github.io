b19d53d0f6252553fa8256d8a2e16f0f2a6ef3cb
hao <hao@hao.com>
2020-08-17 10:30:37 +0800

    Site updated: 2020-08-17 10:30:17

--- preview.sh
+++ preview.sh
@@ -1,3 +1,5 @@
+python handle_img_path.py del
 hexo clean
 hexo g
 hexo s
+python handle_img_path.py add
--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
+python handle_img_path.py del
 hexo clean
 hexo g
 hexo d
+python handle_img_path.py add
